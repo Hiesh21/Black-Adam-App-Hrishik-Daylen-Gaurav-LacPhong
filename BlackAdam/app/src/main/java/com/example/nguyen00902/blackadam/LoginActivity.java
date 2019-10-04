749d4d5796a6d78c9977158f597963afe5cc77a0
Lac-Phong Nguyen <Nguyen00902>
2019-10-04 09:49:54 -0700

    added genre activity and implemented button mechanic to get from one activity to another

--- BlackAdam/app/src/main/java/com/example/nguyen00902/blackadam/LoginActivity.java
+++ BlackAdam/app/src/main/java/com/example/nguyen00902/blackadam/LoginActivity.java
@@ -3,6 +3,7 @@ package com.example.nguyen00902.blackadam;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -145,7 +146,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
      */
     private void attemptLogin() {
         if (mAuthTask != null) {
-            return;
+            startActivity(new Intent(LoginActivity.this, Genre_Screen.class));
         }
 
         // Reset errors.
